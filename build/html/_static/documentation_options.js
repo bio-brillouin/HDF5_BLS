8291948c15611aeaad59c5da7ba116f5b9407d4a
PierreBouvet <pierre.bouvet@meduniwien.ac.at>
2024-11-08 15:06:20 +0100

    Update docs

--- build/html/_static/documentation_options.js
+++ build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '08/11/2024',
-    LANGUAGE: 'Python',
+    VERSION: '0.0.5',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
