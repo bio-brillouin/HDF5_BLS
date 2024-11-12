702ae6c744f261cea0733c14d26a9e6fb70a84d4
PierreBouvet <pierre.bouvet@meduniwien.ac.at>
2024-11-12 16:26:20 +0100

    test

--- source/conf.py
+++ source/conf.py
@@ -7,8 +7,8 @@ import os
 import sys
 from pathlib import Path
 
-sys.path.append('src/')
-# sys.path.append('../src/')
+sys.path.append('./src/')
+sys.path.append('../src/')
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
